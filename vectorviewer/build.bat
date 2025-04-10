56b3e8539903af13b7eb04a6ef2db122f5bfa55b
Mario <windowsmario64@gmail.com>
2025-04-10 00:08:38 -0700

    Clean up generated files.

--- vectorviewer/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-
-echo Building executable...
-pyinstaller --clean vectorviewer.spec
-
-echo Done! The executable is in the dist folder.
-pause 
